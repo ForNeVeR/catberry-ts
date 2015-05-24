0c1e41894a11a29d07728dc95f81e4b8076a9523
Friedrich von Never <friedrich@fornever.me>
2015-05-24 20:26:30 +0600

    Use only server-side source code while parsing the documentation. Close #13.

--- Generate-JsDoc.ps1
+++ Generate-JsDoc.ps1
@@ -1,4 +1,4 @@
-$catberryPath = '.\catberry'
+$catberryPath = '.\catberry\lib'
 
 jsdoc -r $catberryPath -d docs
 cmd /c "jsdoc -r $catberryPath -t templates/haruki -d console -q format=xml > catberry.xml"
