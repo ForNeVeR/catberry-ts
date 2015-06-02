fab98ccbf78539bf5958d383001277e54fe41f4e
rexim <reximkut@gmail.com>
2015-06-02 12:20:04 +0600

    Add skeleton for generators module (#4)

--- generators.ts
+++ generators.ts
@@ -4,3 +4,27 @@ import model = require("jsdoc_model");
 export function generateClass(jsdocClass: model.JsdocClass): string {
     return "";
 }
+
+function generateProperty(jsdocProperty: model.JsdocProperty): string {
+    return "nothing";
+}
+
+function generateConstructor(jsdocConstructor: model.JsdocConstructor): string {
+    return "nothing";
+}
+
+function generateMethod(jsdocMethod: model.JsdocFunction): string {
+    return "nothing";
+}
+
+function prefixComment(tabLevel: number, comment: string): string {
+    return "nothing";
+}
+
+function generateVariable(jsdocParameter: model.JsdocParameter): string {
+    return "nothing";
+}
+
+function generateFunctionDocumentation(jsdocFunction: model.JsdocFunction): string {
+    return "nothing";
+}
