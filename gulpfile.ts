48ad5acda51b48fbc3b50bc76d04cfcb6560b67c
rexim <reximkut@gmail.com>
2015-06-03 02:08:40 +0600

    Dirty hack for running the script from gulp (#4)

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,12 @@
 /// <reference path="typings/tsd.d.ts"/>
+/// <reference path="./typings/shelljs/shelljs.d.ts"/>
 
 import gulp = require('gulp');
-import ts = require('gulp-typescript')
+import ts = require('gulp-typescript');
+import shell = require("shelljs");
+
+// OMG! This is so dirty I need to take a shower!
+process.env["NODE_PATH"] = "./build/:" + process.env["NODE_PATH"];
 
 gulp.task('typescript', () => {
     return gulp.src('./src/*.ts').pipe(ts({
@@ -9,3 +14,10 @@ gulp.task('typescript', () => {
     })).pipe(gulp.dest('./build/'));
 });
 
+gulp.task('jsdoc', () => {
+    shell.exec('node build/generate_jsdoc.js')
+});
+
+gulp.task('tsdef', () => {
+    shell.exec('node build/generate_tsdefinitions.js')
+});
