6c03559bff8db604e9f995941374a62a9054a853
rexim <reximkut@gmail.com>
2015-06-03 11:28:09 +0600

    Different delimiter depending on the platform (#4)

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,8 +5,10 @@ import gulp = require('gulp');
 import ts = require('gulp-typescript');
 import shell = require("shelljs");
 
+var isWin = /^win/.test(process.platform);
+
 // OMG! This is so dirty I need to take a shower!
-process.env["NODE_PATH"] = "./build/:" + process.env["NODE_PATH"];
+process.env["NODE_PATH"] = "./build/" + (isWin ? ";" : ":") + process.env["NODE_PATH"];
 
 gulp.task('typescript', () => {
     return gulp.src('./src/*.ts').pipe(ts({
