bfeff16b8569a203bb8f19e0aec7212357c38e3d
rexim <reximkut@gmail.com>
2015-06-04 23:37:26 +0600

    Use only tsd.d.ts in typing references

--- src/generate_jsdoc.ts
+++ src/generate_jsdoc.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/shelljs/shelljs.d.ts"/>
+/// <reference path="../typings/tsd.d.ts"/>
 import shell = require("shelljs");
 
 function generate_doc(module: string, path: string) {
