958699ef27203fb3a8d431a35fd714a6b175a575
Friedrich von Never <friedrich@fornever.me>
2015-06-06 18:59:43 +0600

    Generate initial definitions for all plugins.

--- src/generate_tsdefinitions.ts
+++ src/generate_tsdefinitions.ts
@@ -1,4 +1,21 @@
 import generate_module = require("generate_module");
 
 generate_module.generate("catberry", ["catberry-locator"]);
-generate_module.generate("catberry-locator", []);
+
+var plugins = [
+    'catberry-assets',
+    'catberry-dust',
+    'catberry-handlebars',
+    'catberry-jade',
+    'catberry-l10n',
+    'catberry-l10n-dust-helper',
+    'catberry-l10n-handlebars-helper',
+    'catberry-locator',
+    'catberry-oauth2-client',
+    'catberry-uhr',
+    'catberry-uri'
+];
+
+plugins.forEach((plugin) => {
+	generate_module.generate(plugin, []);
+});
