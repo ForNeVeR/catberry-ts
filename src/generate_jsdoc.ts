117c1c914d37a5731aa185c2112a3033c2689259
Friedrich von Never <friedrich@fornever.me>
2015-06-06 18:50:16 +0600

    Generate modules documentation.

--- src/generate_jsdoc.ts
+++ src/generate_jsdoc.ts
@@ -1,7 +1,8 @@
 /// <reference path="../typings/tsd.d.ts"/>
 import shell = require("shelljs");
 
-function generate_doc(module: string, path: string) {
+function generate_doc(module: string) {
+    var path = `./${module}/lib`;
     var moduleXml = module + ".xml"
     var jsdoc = `jsdoc -r ${path} -t templates/haruki -d console -q format=xml > ${moduleXml}`;
     shell.exec(jsdoc, function(code, output) {
@@ -14,5 +15,19 @@ function generate_doc(module: string, path: string) {
     });
 }
 
-generate_doc("catberry", "./catberry/lib");
-generate_doc("catberry-locator", "./catberry-locator/lib");
+var modules = [
+    'catberry',
+    'catberry-assets',
+    'catberry-dust',
+    'catberry-handlebars',
+    'catberry-jade',
+    'catberry-l10n',
+    'catberry-l10n-dust-helper',
+    'catberry-l10n-handlebars-helper',
+    'catberry-locator',
+    'catberry-oauth2-client',
+    'catberry-uhr',
+    'catberry-uri'
+];
+
+modules.forEach(generate_doc);
