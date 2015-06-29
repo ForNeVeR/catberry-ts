928f3870b255d46df58896cf4f3eaef1b7d88e85
Friedrich von Never <friedrich@fornever.me>
2015-06-29 22:32:09 +0600

    Import catberry-jade definitions from the browser directory instead of the default lib.

--- definitions/catberry-jade.d.ts
+++ definitions/catberry-jade.d.ts
@@ -1,5 +1,35 @@
-declare module 'catberry-jade' {
-	import Catberry = require('catberry');
+/// <reference path="../typings/tsd.d.ts"/>
 
-	export function register(locator: Catberry.ILocator);
+declare module CatberryJade {
+
+	class TemplateProvider {
+
+
+		/**
+		 * Creates new instance of Jade template provider.
+		 * @param $jade Jade factory.
+		 * 
+		 */
+		TemplateProvider($jade: Jade);
+
+		/**
+		 * Registers compiled (precompiled) Jade template.http://jadejs.com/reference.html
+		 * @param name Template name.
+		 * @param compiled Compiled template source.
+		 * 
+		 */
+		registerCompiled(name: String, compiled: String): any;
+
+		/**
+		 * Renders template with specified data.
+		 * @param name Name of template.
+		 * @param data Data context for template.
+		 * 
+		 */
+		render(name: String, data: Object): any;
+	}
+}
+
+declare module 'catberry-jade' {
+    export = CatberryJade;
 }
--- src/generate_jsdoc.ts
+++ src/generate_jsdoc.ts
@@ -1,33 +1,43 @@
 /// <reference path="../typings/tsd.d.ts"/>
-import shell = require("shelljs");
+import shell = require('shelljs');
+import path = require('path');
 
-function generate_doc(module: string) {
-    var path = `./${module}/lib`;
-    var moduleXml = module + ".xml"
-    var jsdoc = `jsdoc -r ${path} -t templates/haruki -d console -q format=xml > ${moduleXml}`;
+class Module {
+    name: string;
+    directory: string;
+    constructor(name: string, directory: string = 'lib') {
+        this.name = name;
+        this.directory = directory;
+    }
+}
+
+function generate_doc(module: Module) {
+    var modulePath = path.join('.', module.name, module.directory);
+    var moduleXml = module.name + ".xml"
+    var jsdoc = `jsdoc -r ${modulePath} -t templates/haruki -d console -q format=xml > ${moduleXml}`;
     shell.exec(jsdoc, function(code, output) {
         if (code == 0) {
-            console.log(module + ".xml DONE");
+            console.log(module.name + ".xml DONE");
         } else {
-            console.log(module + ".xml FAIL");
+            console.log(module.name + ".xml FAIL");
             shell.rm("-f", moduleXml);
         }
     });
 }
 
 var modules = [
-    'catberry',
-    'catberry-assets',
-    'catberry-dust',
-    'catberry-handlebars',
-    'catberry-jade',
-    'catberry-l10n',
-    'catberry-l10n-dust-helper',
-    'catberry-l10n-handlebars-helper',
-    'catberry-locator',
-    'catberry-oauth2-client',
-    'catberry-uhr',
-    'catberry-uri'
+    new Module('catberry'),
+    new Module('catberry-assets'),
+    new Module('catberry-dust'),
+    new Module('catberry-handlebars'),
+    new Module('catberry-jade', 'browser'),
+    new Module('catberry-l10n'),
+    new Module('catberry-l10n-dust-helper'),
+    new Module('catberry-l10n-handlebars-helper'),
+    new Module('catberry-locator'),
+    new Module('catberry-oauth2-client'),
+    new Module('catberry-uhr'),
+    new Module('catberry-uri')
 ];
 
 modules.forEach(generate_doc);
